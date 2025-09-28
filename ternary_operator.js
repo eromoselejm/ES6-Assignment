f3a41e2cc63ba56c832ec0e78b82d8ae08131266
eromoselejm <eromoselejm@gmail.com>
2025-09-28 13:45:34 +0100

    Update ternary_operator.js

--- ternary_operator.js
+++ ternary_operator.js
@@ -7,3 +7,6 @@ age >= 18 ? console.log("You are an adult") : console.log("You are a child");
 age = 18;
 const canVote = age >= 18 ? 'You can vote' : 'You cannot vote';
 console.log(canVote);
+
+//Serves as a one line replacement for an if-else statement
+//It's syntax is (condition) ? ifTrue : ifFalse;
